4aa1276a8bd52ca085ca61c6ab087c7404f60a13
StanislawTerziev <standa.terziev@gmail.com>
2020-08-16 23:25:52 +0200

    DB create script

---
+++ Db/create/create.sql
@@ -0,0 +1,10 @@
+insert into JobCategory values
+(1, "Programmer"), (2,"Project Manager"), (3,"Product Manager"), (4,"Delivery Manager");
+
+insert into Country values
+(1, "Czechia"), (2,"Slovakia"), (3,"Germany"), (4,"Poland"), (5,"Austria");
+
+insert into Employee values
+(1, 'Jan', 'Konecny', '1989-03-03', 2, 1,'jkonecny@seznam.cz', '+420721873123', 1, '05-10-2016', '05-31-2019'),
+(2, 'Martin', 'Vrana', '1982-09-20', 2, 4,'martin.vrana@gmail.com', '+420603813193', 1, '07-01-2013', '01-31-2018'),
+(3, 'Dalimil', 'Bohac', '1987-09-20', 2, 2,'dalboh@gmail.com', '+420622723765', 1, '07-01-2015', null)
